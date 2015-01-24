e9b91da894ded53b548f490873c4862d6e74f546
Max Beizer <max.beizer@gmail.com>
2015-01-24 16:03:32 -0600

    Add imperative loop bubble sort

--- RandoSort.playground/section-1.swift
+++ RandoSort.playground/section-1.swift
@@ -1,3 +1,32 @@
 let arrayOfRandos = [3, 2, 5, 4, 1]
+
+// naive
 let sortedRandos = arrayOfRandos.sorted { $0 < $1 }
 println(sortedRandos)
+
+// imperative/looped
+func sortLoopRando(randos: [Int]) -> [Int] {
+    if (randos.count == 1 || randos.isEmpty) {
+        return randos
+    }
+    
+    var loopedRandos: [Int] = randos
+    var swapped: Bool = true
+    while swapped {
+        swapped = false
+        for i in 0..<loopedRandos.count-1 {
+            var left = loopedRandos[i]
+            var right = loopedRandos[i+1]
+            if left > right {
+                loopedRandos[i] = right
+                loopedRandos[i+1] = left
+                swapped = true
+            }
+        }
+    }
+    
+    return loopedRandos
+}
+
+sortLoopRando(arrayOfRandos)
+
